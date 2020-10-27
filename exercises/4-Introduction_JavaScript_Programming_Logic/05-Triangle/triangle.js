3d7235cc8cafa2ffe6b4a9a24be73114c94a481c
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2020-10-27 13:44:50 -0300

    Resoluçao parcial do exercicio 5

---
+++ exercises/4-Introduction_JavaScript_Programming_Logic/05-Triangle/triangle.js
@@ -0,0 +1,17 @@
+// let sideA = 10;
+// let sideB = 20;
+// let sideC = 30;
+
+
+
+
+let angleA = 30;
+let angleB = 50;
+let angleC = 100;
+
+// let scaleTriangle;
+// let isocelesTriangle;
+// let equilateralTriangle;
+
+
+
